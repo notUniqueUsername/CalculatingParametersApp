1e3c717b4c72d22ea787bb6fef740e9ae619202f
notUniqueUsername <muternatallia@gmail.com>
2020-05-20 19:33:47 +0700

    еще якоря

--- ParametersApp/Form1.Designer.cs
+++ ParametersApp/Form1.Designer.cs
@@ -416,6 +416,9 @@
             // 
             // pictureBoxUp
             // 
+            this.pictureBoxUp.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBoxUp.Image = global::ParametersApp.Properties.Resources.String_up;
             this.pictureBoxUp.Location = new System.Drawing.Point(12, 27);
             this.pictureBoxUp.Name = "pictureBoxUp";
@@ -426,6 +429,9 @@
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Image = global::ParametersApp.Properties.Resources.String_low_;
             this.pictureBox1.Location = new System.Drawing.Point(12, 136);
             this.pictureBox1.Name = "pictureBox1";
